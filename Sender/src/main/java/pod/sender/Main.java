739b5e7d245c4e7416dc7102f25edf44bc6ab82b
Jéderson Gonçalves <jedersongmoura@gmail.com>
2016-05-09 19:52:32 -0300

    MOdificações

---
+++ Sender/src/main/java/pod/sender/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pod.sender;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import pod.receiver.Receiver;
+
+/**
+ *
+ * @author Maluco Beleza
+ */
+public class Main {
+    
+    public static void main(String[] args) throws RemoteException, NotBoundException {
+        Sender send = new SenderImpl();
+        send.commandFromClient();
+  
+//   Registry registry = LocateRegistry.getRegistry("localhost", 8475);
+//   Receiver receiver = (Receiver) registry.lookup("Receiver");
+//   String result = receiver.singin("jederson@gmail.com");
+//   System.out.println(result);
+    
+    }
+}
