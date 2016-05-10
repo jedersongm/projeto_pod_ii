f3f30e00add3a9fca0df1046bfcdbad5ead26c52
Jéderson Gonçalves <jedersongmoura@gmail.com>
2016-05-09 22:37:11 -0300

    ajsutes

--- ClientApp/index.php
+++ ClientApp/index.php
@@ -7,21 +7,56 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Chatmania</title>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
-        <h3>Seridó Mania</h3>
-        <form action="cadastro.php" method="POST">
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required/>
-            <br>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required/>
-            <br>
-            <button type="submit" value="Cadastrar">Cadastar</button>
-        </form>
+
+        <div class="jumbotron">
+            <div class="row">
+                <div class="col-md-5">
+                    <h3>Login</h3>
+                    <form action="login.php" method="post" role="form" class="form-inline">
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="email" class="form-control" id="email">
+                        </div>
+                        <button type="submit" value="Login">Login</button>
+                    </form>
+                </div>
+                <div class="col-md-5">
+                    <h3>Cadastre-se</h3>
+                    <form action="cadastro.php" method="POST">
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" id="name" name="name" class="form-control" required/>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="email" id="email" name="email" class="form-control"required/>
+                        </div>
+                        <button type="submit" value="Cadastrar">Cadastar</button>
+                    </form>
+                </div>
+            </div>            
+        </div>
     </body>
 </html>
+
